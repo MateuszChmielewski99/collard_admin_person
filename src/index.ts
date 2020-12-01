45acf005a52a096f620b06c96a130104314f17f0
MateuszChmielewski99 <chmielewski079@gmail.com>
2020-12-01 20:28:52 +0100

    added healthcheck endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import 'reflect-metadata';
 import DistributorRouter from './controllers/DirectorController';
 import { bootstrap } from './container-setup';
@@ -9,7 +9,12 @@ bootstrap();
 const app: Application = express();
 
 app.use('/director', DistributorRouter);
-app.use('/actor',ActorRouter)
+app.use('/actor',ActorRouter);
+
+//healthcheck
+app.get('/', (_req: Request, res: Response) => {
+    return res.sendStatus(200);
+  });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('App and running'));
